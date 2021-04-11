369e29e0eff8fd3f095a8199221d2d23d416c10e
Loren Vanderbilt <JoshKBaum@gmail.com>
2021-04-11 17:35:33 -0500

    basic layout

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './home.css'
+import React, {useState} from 'react';
+import Splatter from '../../resources/images/Splatter.jpg';
+import Socials from '../socials/socials.js';
+import './home.css';
 
-export default function Home(props){
-    return(
-        <div className={'homeContainer'}>
-            HUMDRUM
-        </div>
-    )
+export default function Home(props) {
+    const [hover, toggleHover]=useState(false)
+  return (
+    <div
+      className={'homeContainer'}
+      style={{
+        backgroundImage: `url(${Splatter})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+      }}
+      onMouseEnter={()=> toggleHover(true)}
+      onMouseLeave={()=> toggleHover(false)}
+    >
+        {hover && <Socials />}
+      HUMDRUM
+    </div>
+  );
 }
