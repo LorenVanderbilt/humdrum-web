6ad23a7310098e1104bacdc00a241639ea0e85ed
Loren Vanderbilt <JoshKBaum@gmail.com>
2021-04-11 16:07:10 -0500

    initial commit

---
+++ src/components/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.css'
+
+export default function Home(props){
+    return(
+        <div className={'homeContainer'}>
+            HUMDRUM
+        </div>
+    )
+}
