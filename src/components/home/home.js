c580a0540be8e5553d77b4ba1bf8975bef77270d
Loren Vanderbilt <JoshKBaum@gmail.com>
2021-05-23 13:45:19 -0500

    fix social animation, hide main body work

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import Splatter from '../../resources/images/Splatter.jpg';
 import Socials from '../socials/Socials.js';
-import { Parallax } from 'react-scroll-parallax';
+import Bio from '../bio/Bio';
+// import { Parallax } from 'react-scroll-parallax';
 import './Home.css';
 
 export default function Home(props) {
-  const [hover, toggleHover] = useState(false);
+  const [section, setSection] = useState(null);
+
+  const homeOptions = ['Bio', 'Shows', 'Store', 'Contact'];
+  // console.log('SECTION', section)
+
+  const moveMenu =()=> {
+    // document.getElementById('img').className = 'classname';
+    document.getElementById('home-header').classList.add('moveUp')
+  }
+
   return (
     <div
       className={'homeContainer'}
@@ -16,26 +26,29 @@ export default function Home(props) {
         backgroundRepeat: 'no-repeat',
         width: '100vw',
         height: '100vh',
+        overflow: 'scroll',
       }}
-      onMouseEnter={() => toggleHover(true)}
-      onMouseLeave={() => toggleHover(false)}
     >
-      {/* {hover && <Socials />} */}
       <Socials />
-
-      {/* <Parallax className="custom-class"
-       offsetYMax={100}
-       offsetYMin={-120}
-       slowerScrollRate
-        tagOuter="figure"> */}
+      <div className={'homeMenuContainer'} id={'home-header'}>
         <div className={'headerText'}>HUMDRUM</div>
-      {/* </Parallax> */}
-      <div className={'homeMenuContainer'}>
-        <span>Bio</span>
-        <span>Shows</span>
-        <span>Store</span>
-        <span>Contact</span>
+        {/* <div className={'homeMenuOptionsContainer'}>
+          {homeOptions.map((option, index) => {
+            return (
+              <span
+                onClick={() => {setSection(option);moveMenu()}}
+                key={index}
+                className={'homeMenuOption'}
+              >
+                {option}
+              </span>
+            );
+          })}
+        </div> */}
       </div>
+      {/* {section && (
+        <div className={'homeBodyContent'}>{section === 'Bio' && <Bio />}</div>
+      )} */}
     </div>
   );
 }
