1bf1dd7d8d010d8b899e1aa6bd0f911aa219a459
Loren Vanderbilt <JoshKBaum@gmail.com>
2021-10-10 16:57:08 -0500

    added apple and spotify links

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -3,18 +3,20 @@ import Twitter from '../../resources/images/twitter.svg';
 import Facebook from '../../resources/images/facebook.svg';
 import Instagram from '../../resources/images/instagram.svg';
 import Tiktok from '../../resources/images/tiktok.svg';
-import Soundcloud from '../../resources/images/soundcloud.svg';
 import Bandcamp from '../../resources/images/bandcamp.svg';
+import Apple from '../../resources/images/apple.svg';
+import Spotify from '../../resources/images/spotify.svg';
+
 import './socials.css';
 
 const social = [
   { image: Bandcamp, url: 'https://heyhumdrum.bandcamp.com/releases' },
+  { image: Spotify, url: 'https://open.spotify.com/album/4Ipdloec3fPT77HqDN70AJ' },
+  { image: Apple, url: 'https://music.apple.com/us/album/wave-goodbye-single/1588532279?uo=4&app=itunes' },
   { image: Instagram, url: 'http://instagram.com/heyhumdrum' },
   { image: Twitter, url: 'http://twitter.com/heyhumdrum' },
-  // { image: Soundcloud, url: 'http://soundcloud.com/heyhumdrum' },
   { image: Tiktok, url: 'http://tiktok.com/@heyhumdrumm' },
   { image: Facebook, url: 'http://www.facebook.com/heyhumdrum' },
-
 ];
 
 export default function Socials(props) {
