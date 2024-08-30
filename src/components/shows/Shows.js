6d7fed881c83b5b49aab06cf5809aea7a88d60f2
Loren Vanderbilt <JoshKBaum@gmail.com>
2024-08-29 22:45:54 -0500

    garbage collecting and updated text style for shows

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -8,13 +8,12 @@ export default function Shows(props) {
       <div className={'shows-container'}>
         <div className={'shows-header-text'}>shows</div>
         <div className={'shows-body'}>
-          <div className={'shows-sub-text'} style={{marginBottom: '15px'}}>U p c o m i n g</div>
+          <div className={'shows-sub-text'} style={{marginBottom: '15px'}}>u p c o m i n g</div>
           {/* <div style={{marginBottom: '50px'}}>August 6, 2024 - Empty Bottle - Chicago, Il</div> */}
-
           {/* <img src={image} className={'flyer'} alt={'flyer for upcoming show'}/> */}
           {/* <div><a href="https://www.etix.com/ticket/p/5087751/tallies-torontosunshyhumdrum-chicago-coles-bar" target="_blank" rel="noopener noreferrer">June 22, 2023 - Cole's - Chicago, Il</a></div> */}
           <div>No live performances scheduled at this time.</div>
-          <div className={'shows-sub-text blue'}  style={{marginBottom: '15px'}}>P r e v i o u s</div>
+          <div className={'shows-sub-text blue'}  style={{marginBottom: '15px'}}>p r e v i o u s</div>
           <div>August 6, 2024 - Empty Bottle - Chicago, Il</div>
           <div>June 12, 2024 - Fallen Log - Chicago, Il</div>
           <div>March 8, 2024 - Burlington - Chicago, Il</div>
