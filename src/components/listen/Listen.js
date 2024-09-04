4a83ee731712fa517a2286a861df846e97c551ed
Loren Vanderbilt <JoshKBaum@gmail.com>
2024-09-04 18:42:52 -0500

    updated listen page with new single

--- src/components/listen/Listen.js
+++ src/components/listen/Listen.js
@@ -5,16 +5,16 @@ export default function Listen(props) {
     <div id={'Listen-Section'}>
       <div className={'listen-container'}>
         <div className={'listen-header-text'}>listen</div>
+
         <iframe 
-        style={{ border: 0, width: '350px', height: '470px', marginBottom: '30px' }}
-        src="https://bandcamp.com/EmbeddedPlayer/album=3252435323/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+        style={{ border: 0, width: '350px', height: '622px', marginBottom: '30px' }}
+        src="https://bandcamp.com/EmbeddedPlayer/album=3252435323/size=large/bgcol=ffffff/linkcol=0687f5/track=863815641/transparent=true/" 
         seamless
         title="bandcamp widget"
         >
-          <a href="https://heyhumdrum.bandcamp.com/album/every-heaven">
-            Every Heaven by Humdrum
-          </a>
-        </iframe>
+          <a href="https://heyhumdrum.bandcamp.com/album/every-heaven">Every Heaven by Humdrum</a>
+          </iframe>
+       
         <iframe
           style={{ border: 0, width: '350px', height: '442px', marginBottom: '30px' }}
           src="https://bandcamp.com/EmbeddedPlayer/track=1650045633/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
