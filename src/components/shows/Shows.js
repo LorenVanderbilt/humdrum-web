79e398e7fe6387001bf9f6d711c09e1c94850a10
Loren Vanderbilt <JoshKBaum@gmail.com>
2025-04-28 21:23:44 -0500

    update shows

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -9,15 +9,16 @@ export default function Shows(props) {
         <div className={'shows-header-text'}>shows</div>
         <div className={'shows-body'}>
           <div className={'shows-sub-text'} style={{marginBottom: '15px'}}>u p c o m i n g</div>
-          <div><a href="https://lh-st.com/shows/02-16-2025-emily-jane-powers/" target="_blank" rel="noopener noreferrer">February 16, 2025 - Schubas - Chicago, Il</a></div>
-          <div><a href="https://www.newcolossusfestival.com/badges" target="_blank" rel="noopener noreferrer">March, 2025 - The New Colossus Festival - New York, NY (details TBA)</a></div>
-          <div>March 28, 2025 - 7th Street Space - Dekalb, Il</div>
-          <div><a href="https://kitchen17.com/events/" target="_blank" rel="noopener noreferrer">March 29, 2025 - Fallen Log - Chicago, Il</a></div>
           {/* <div style={{marginBottom: '50px'}}>August 6, 2024 - Empty Bottle - Chicago, Il</div> */}
           {/* <img src={image} className={'flyer'} alt={'flyer for upcoming show'}/> */}
           {/* <div><a href="https://www.newcolossusfestival.com/badges" target="_blank" rel="noopener noreferrer">June 22, 2023 - Cole's - Chicago, Il</a></div> */}
-          {/* <div>No live performances scheduled at this time.</div> */}
+          <div>No live performances scheduled at this time.</div>
           <div className={'shows-sub-text blue'}  style={{marginBottom: '15px'}}>p r e v i o u s</div>
+          <div>March 29, 2025 - Fallen Log - Chicago, Il</div>
+          <div>March 28, 2025 - 7th Street Space - Dekalb, Il</div>
+          <div>March 8, 2025 - Baker Falls, The New Colossus Festival - New York, NY</div>
+          <div>March 7, 2025 - Baker Falls, The New Colossus Festival - New York, NY</div>
+          <div>February 16, 2025 - Schubas - Chicago, Il</div>
           <div>December 7, 2024 - Tone Deaf Records - Chicago, Il</div>
           <div>November 6, 2024 - Cobra Lounge - Chicago, Il</div>
           <div>October 18, 2024 - Album Listening Party + DJ night w/ Smashed Plastic @ Reed's Local</div>
