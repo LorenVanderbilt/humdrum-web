4860286bb1e455755c36fb6a45f28184caa461ed
Loren Vanderbilt <JoshKBaum@gmail.com>
2025-07-23 21:10:41 -0500

    update shows and css

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -9,13 +9,13 @@ export default function Shows(props) {
         <div className={'shows-header-text'}>shows</div>
         <div className={'shows-body'}>
           <div className={'shows-sub-text'} style={{marginBottom: '15px'}}>u p c o m i n g</div>
-          <div>June 14, 2025 - Bric-a-Brac Records - Chicago, Il</div>
-          <div>June 20, 2025 - Metro - Chicago, Il</div>
           {/* <div style={{marginBottom: '50px'}}>August 6, 2024 - Empty Bottle - Chicago, Il</div> */}
           {/* <img src={image} className={'flyer'} alt={'flyer for upcoming show'}/> */}
           {/* <div><a href="https://www.newcolossusfestival.com/badges" target="_blank" rel="noopener noreferrer">June 22, 2023 - Cole's - Chicago, Il</a></div> */}
-          {/* <div>No live performances scheduled at this time.</div> */}
+          <div>No live performances scheduled at this time.</div>
           <div className={'shows-sub-text blue'}  style={{marginBottom: '15px'}}>p r e v i o u s</div>
+          <div>June 20, 2025 - Metro - Chicago, Il</div>
+          <div>June 14, 2025 - Bric-a-Brac Records - Chicago, Il</div>
           <div>March 29, 2025 - Fallen Log - Chicago, Il</div>
           <div>March 28, 2025 - 7th Street Space - Dekalb, Il</div>
           <div>March 8, 2025 - Baker Falls, The New Colossus Festival - New York, NY</div>
